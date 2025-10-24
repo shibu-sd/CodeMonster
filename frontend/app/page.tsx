7924eb5f8cef646019466cb8256d19bb348c6d7f
shibu-sd <sd.shibu007@gmail.com>
2025-10-24 11:45:35 +0530

    Updated index page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero-section/hero-section";
 import FooterSection from "@/components/footer/footer";
+import { Testimonials } from "@/components/testimonials";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <div>
             <HeroSection />
+            <Testimonials />
             <FooterSection />
         </div>
     );
